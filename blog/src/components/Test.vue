c41a7551eb4463c55fadd0d36279c43a01d3e1e1
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-18 20:17:59 +0530

    vue templete is added

---
+++ blog/src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello from the Home component</h1>
+    <h2>{{ data }}</h2>
+    <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  props: {
+    data: String,
+    msg: String,
+  },
+};
+</script>
+<style></style>
