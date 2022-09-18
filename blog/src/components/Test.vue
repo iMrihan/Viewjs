fbabbe25cae3e580232d0430fb037fb64225c654
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-18 21:22:16 +0530

    for loop is created in vue.js

--- blog/src/components/Test.vue
+++ blog/src/components/Test.vue
@@ -5,9 +5,23 @@
     <h3>{{ msg }}</h3>
     <button v-on:click="hello('call hello')">Call hello Function</button> -->
 
-    <h1 v-if="show">Conditional Rerendering in vue.js</h1>
+    <!-- <h1 v-if="show">Conditional Rerendering in vue.js</h1>
     <h1 v-else>Conditional Rerendering in vue.js else block</h1>
-    <button v-on:click="display">Toggle Element</button>
+    <button v-on:click="display">Toggle Element</button> -->
+
+    <table border="1px">
+      <tr>
+        <td>Id</td>
+        <td>Name</td>
+        <td>Email</td>
+      </tr>
+
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -18,8 +32,17 @@ export default {
     data: String,
     msg: String,
   },
+  // data() {
+  //   return { show: true };
+  // },
   data() {
-    return { show: true };
+    return {
+      users: [
+        { id: 1, name: "peter", email: "peter@gmail.com" },
+        { id: 2, name: "sam", email: "sam@gmail.com" },
+        { id: 3, name: "brute", email: "brute@gmail.com" },
+      ],
+    };
   },
   methods: {
     // hello(data) {
