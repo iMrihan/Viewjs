76f0c96fbc9c5df527560a2dd4914a0bb82880fd
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-20 15:06:31 +0530

    added some more assignment

---
+++ PropsInVuejs/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>Props in Vue js</h1>
+  <!-- <Child name="Rihan Khan" /> -->
+  <!-- <Child v-bind:name="userName" /> -->
+
+  <Child v-bind:users="users" />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Child from "./components/Child.vue";
+
+export default {
+  name: "App",
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      users: [
+        { name: "Rihan", email: "rihan@gmail.com" },
+        { name: "Praveen", email: "praveen@gmail.com" },
+        { name: "Ashish", email: "ashish@gmail.com" },
+        { name: "Sonu", email: "sonu@gmail.com" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ PropsInVuejs/src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Child Component</h1>
+    <h2 v-for="user in users" :key="user.name">
+      Name: {{ user.name }} && Email: {{ user.email }}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  props: ["users"],
+};
+</script>
+
+<style></style>
