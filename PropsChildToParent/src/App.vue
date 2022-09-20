d4c200c10e9a76ac48b30699d2a2c83e196f8649
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-20 15:44:57 +0530

    added how to send data child to parent

---
+++ PropsChildToParent/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>Send Props Child to Parent</h1>
+  <h2>{{ title }}</h2>
+  <Child v-on:changeTitle="updateTitle($event)" />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Child from "./components/Child.vue";
+
+export default {
+  name: "App",
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      title: "Props Tutorial",
+    };
+  },
+  methods: {
+    updateTitle(title) {
+      this.title = title;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ PropsChildToParent/src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button v-on:click="updateTitle">Update Parent Title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  methods: {
+    updateTitle() {
+      this.$emit("ChangeTitle", "Parent Updated");
+    },
+  },
+};
+</script>
+
+<style></style>
