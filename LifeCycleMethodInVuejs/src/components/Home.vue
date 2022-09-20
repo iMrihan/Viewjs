bf6df6c8f9e2283f47a01c995ea5d57577d1010c
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-20 16:35:42 +0530

    life cycle method added in new project

---
+++ LifeCycleMethodInVuejs/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div><h1>Home</h1></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  beforeMount: function () {
+    console.warn("beforeMount");
+  },
+  mounted: function () {
+    console.warn("mounted");
+  },
+  created: function () {
+    console.warn("created");
+  },
+  beforeCreate: function () {
+    console.warn("beforeCreated");
+  },
+};
+</script>
+
+<style></style>
