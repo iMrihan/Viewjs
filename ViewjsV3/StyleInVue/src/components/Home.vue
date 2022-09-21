2e4f8e722029c22a4e41d398de3ae0a3d2eccae4
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-21 17:48:22 +0530

    added style features

--- ViewjsV3/StyleInVue/src/components/Home.vue
+++ ViewjsV3/StyleInVue/src/components/Home.vue
@@ -1,11 +1,45 @@
 <template>
-  <h1>Style In Vue Js with Dynamically</h1>
+  <div>
+    <h1>Style In Vue Js with Dynamically</h1>
+
+    <!-- <h2 :class="{ green: colorChange, err: true, other: true }">component</h2> -->
+    <h2 :class="applyStyle">component</h2>
+
+    <button v-on:click="colorChange = !colorChange">Apply Style</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+
+  data() {
+    return {
+      colorChange: false,
+    };
+  },
+
+  computed: {
+    applyStyle() {
+      return {
+        green: this.colorChange,
+        err: true,
+        other: true,
+      };
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+.green {
+  background: green;
+  width: 300px;
+}
+.err {
+  color: red;
+}
+.other {
+  font-size: 45px;
+}
+</style>
