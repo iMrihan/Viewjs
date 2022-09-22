01bde6f957a7144ee65c7b30eb4df95020ee455c
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-22 11:38:36 +0530

    Ref features is added

---
+++ VuejsV3/Ref/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Ref in Vue Js</h1>
+    <input type="text" ref="input" />
+    <button v-on:click="getData">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+    getData() {
+      this.$refs.input.focus();
+      let val = this.$refs.input.value;
+      this.$refs.input.style.color = "red";
+      console.log(val);
+    },
+  },
+};
+</script>
+
+<style></style>
