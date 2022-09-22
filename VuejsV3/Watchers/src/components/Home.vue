77b508683171afc629ebcd3c07fa43d00b1a3d7e
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-22 14:29:09 +0530

    added watches property

---
+++ VuejsV3/Watchers/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Watchers Property in Vue Js</h1>
+    <h2>{{ count }}</h2>
+    <button v-on:click="count = count + 1">+</button>
+    <button v-on:click="count = count - 1">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  watch: {
+    count(cur, prev) {
+      if (cur > 5 && prev < cur) {
+        // alert("stop counting")
+        this.count = 0;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
