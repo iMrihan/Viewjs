8a4505c0dc95a5a94de23ce353a39d441b0f0fac
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-22 14:01:20 +0530

    added computed property

---
+++ VuejsV3/ComputedProperty/src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Computed Property</h1>
+    <h2>{{ dollors * rupeesVal - discount }}</h2>
+    <h2>{{ getResult }}</h2>
+    <h2>{{ getResultMethod() }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      dollors: 100,
+      rupeesVal: 70,
+      discount: 10,
+    };
+  },
+
+  methods: {
+    getResultMethod() {
+      return this.dollors * this.rupeesVal - this.discount;
+    },
+  },
+
+  // we use computed property for complicated calculation
+  computed: {
+    getResult() {
+      return this.dollors * this.rupeesVal - this.discount;
+    },
+  },
+};
+</script>
+
+<style></style>
