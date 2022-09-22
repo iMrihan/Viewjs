091933234c05d805b7292dd06bddf1e1eb4966f9
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-22 19:13:19 +0530

    added teleport component

---
+++ VuejsV3/TeleportComponent/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <h2>Footer</h2>
+    <p>copy right @ Code Step By Step</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+.footer {
+  /* border: 1px solid red; */
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  left: 0;
+  background: aqua;
+  padding-left: 10%;
+}
+</style>
---
+++ VuejsV3/TeleportComponent/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Home Component</h1>
+
+    <teleport to="#footer">
+      <Footer />
+    </teleport>
+  </div>
+</template>
+
+<script>
+import Footer from "./Footer.vue";
+export default {
+  name: "Home",
+  components: {
+    Footer,
+  },
+};
+</script>
+
+<style></style>
