e39819eb04ccf8aed213c42433abf1c1ebb112b3
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-22 12:54:10 +0530

    Form Modifiers

---
+++ VuejsV3/FormMidifiers/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Home Component</h1>
+    <!-- <input type="text" v-model.lazy="data" /> -->
+    <input type="number" v-model.number="data" />
+
+    <h2>Data is : {{ data }}</h2>
+
+    <h2>Data is : {{ typeof data }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      data: "",
+    };
+  },
+};
+</script>
+
+<style></style>
