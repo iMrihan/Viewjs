b50ead4f4ad9826805220ec2cf6b479bfc8b209c
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-24 01:18:07 +0530

    added avatar in vue js using axios with the help of api call

---
+++ VuejsV3/ApiCall/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Home Component</h1>
+    <p v-for="item in list" :key="item">
+      {{ item.id }} --- {{ item.email }}---- {{ item.first_name }} -----{{
+        item.last_name
+      }}
+      <img :src="item.avatar" alt="" />
+    </p>
+  </div>
+</template>
+
+<script>
+import { assertDeclareModuleExports } from "@babel/types";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      list: [],
+    };
+  },
+
+  async mounted() {
+    let { data } = await axios.get("https://reqres.in/api/users?page=1");
+    console.log(data.data);
+    this.list = data.data;
+  },
+};
+</script>
+
+<style></style>
