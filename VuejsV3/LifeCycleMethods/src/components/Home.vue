d2bd6fbfced5301eb76f583ab3dc3c0d054f6a1b
iMrihan <mhd.khan.rihan@gmail.com>
2022-09-23 19:55:22 +0530

    life cycle methods is added

---
+++ VuejsV3/LifeCycleMethods/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Home Component</h1>
+    <button v-on:click="show = !show">Toggle Component</button>
+    <User v-if="show" />
+  </div>
+</template>
+
+<script>
+import User from "./User.vue";
+export default {
+  name: "Home",
+  data() {
+    return {
+      show: true,
+    };
+  },
+  components: {
+    User,
+  },
+};
+</script>
+
+<style></style>
---
+++ VuejsV3/LifeCycleMethods/src/components/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 ref="user">User Component</h1>
+</template>
+
+<script>
+export default {
+  name: "User",
+
+  beforeUnmount() {
+    console.log("beforeUnmount", this.$refs["user"]);
+  },
+  unmounted() {
+    console.log("Unmount", this.$refs["user"]);
+  },
+};
+</script>
+
+<style></style>
